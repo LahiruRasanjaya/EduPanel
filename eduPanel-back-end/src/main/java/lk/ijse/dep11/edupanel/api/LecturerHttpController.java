498f945350ea50c36b4d01c298e196734362671a
Lahiru Rasanjaya <phlrasanjaya@gmail.com>
2024-01-17 22:42:30 +0530

    complete LecturerRequestTO

--- eduPanel-back-end/src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
+++ eduPanel-back-end/src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
@@ -1,6 +1,8 @@
 package lk.ijse.dep11.edupanel.api;
 
+import lk.ijse.dep11.edupanel.to.request.LecturerReqTO;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,7 +12,8 @@ public class LecturerHttpController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = "multipart/form-data", produces = "application/json")
-    public void createNewLecturer(){}
+    public void createNewLecturer(@ModelAttribute @Validated(LecturerReqTO.Create.class)
+                                      LecturerReqTO lecturerReqTo){}
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping(value = "/{lecturer-id}", consumes = "multipart/form-data")

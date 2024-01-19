8cb79d12273776675da46836db872e7357c562ee
Lahiru Rasanjaya <phlrasanjaya@gmail.com>
2024-01-19 21:33:59 +0530

    implement createNewLecturer method

--- eduPanel-back-end/src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
+++ eduPanel-back-end/src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
@@ -1,19 +1,65 @@
 package lk.ijse.dep11.edupanel.api;
 
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import lk.ijse.dep11.edupanel.entity.Lecturer;
+import lk.ijse.dep11.edupanel.entity.LinkedIn;
+import lk.ijse.dep11.edupanel.entity.Picture;
+import lk.ijse.dep11.edupanel.to.LecturerTO;
 import lk.ijse.dep11.edupanel.to.request.LecturerReqTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityManager;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/v1/lecturers")
 @CrossOrigin
 public class LecturerHttpController {
+    @Autowired
+    private EntityManager em;
+    @Autowired
+    private ModelMapper mapper;
+    @Autowired
+    private Bucket bucket;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = "multipart/form-data", produces = "application/json")
-    public void createNewLecturer(@ModelAttribute @Validated(LecturerReqTO.Create.class)
-                                      LecturerReqTO lecturerReqTo){}
+    public LecturerTO createNewLecturer(@ModelAttribute @Validated(LecturerReqTO.Create.class)
+                                      LecturerReqTO lecturerReqTo){
+        em.getTransaction().begin();
+        try {
+            Lecturer lecturer = mapper.map(lecturerReqTo, Lecturer.class);
+            lecturer.setPicture(null);
+            lecturer.setLinkedIn(null);
+            em.persist(lecturer);
+            LecturerTO lecturerTO = mapper.map(lecturer, LecturerTO.class);
+
+            if (lecturerReqTo.getLinkedin() != null) {
+                em.persist(new LinkedIn(lecturer, lecturerReqTo.getLinkedin()));
+                lecturerTO.setLinkedin(lecturerReqTo.getLinkedin());
+            }
+
+            if (lecturerReqTo.getPicture() != null) {
+                Picture picture = new Picture(lecturer, "lecturers/" + lecturer.getId());
+                em.persist(picture);
+
+                Blob blobRef = bucket.create(picture.getPicturePath(), lecturerReqTo.getPicture().getInputStream(), lecturerReqTo.getPicture().getContentType());
+                lecturerTO.setPicture(blobRef.signUrl(1, TimeUnit.DAYS, Storage.SignUrlOption.withV4Signature()).toString());
+            }
+
+            em.getTransaction().commit();
+            return lecturerTO;
+        } catch (Throwable t) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(t);
+        }
+    }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping(value = "/{lecturer-id}", consumes = "multipart/form-data")

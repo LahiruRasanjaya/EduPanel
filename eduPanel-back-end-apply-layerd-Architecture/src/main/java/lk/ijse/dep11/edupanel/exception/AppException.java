2fc137a545a09086df806ade62915404fe1c931a
Lahiru Rasanjaya <phlrasanjaya@gmail.com>
2024-01-24 15:06:46 +0530

    Create AppExceptions

---
+++ eduPanel-back-end-apply-layerd-Architecture/src/main/java/lk/ijse/dep11/edupanel/exception/AppException.java
@@ -0,0 +1,29 @@
+package lk.ijse.dep11.edupanel.exception;
+
+public class AppException extends RuntimeException{
+    private int errorCode;
+
+    public AppException(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public AppException(String message, int errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public AppException(String message, Throwable cause, int errorCode) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+
+    public AppException(Throwable cause, int errorCode) {
+        super(cause);
+        this.errorCode = errorCode;
+    }
+
+    public AppException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, int errorCode) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.errorCode = errorCode;
+    }
+}

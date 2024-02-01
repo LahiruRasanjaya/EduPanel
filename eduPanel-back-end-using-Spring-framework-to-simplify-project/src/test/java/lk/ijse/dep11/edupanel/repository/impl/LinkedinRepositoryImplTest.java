90b9cd664eaebf0490d21b159e7a3d73644eb372
Lahiru Rasanjaya <phlrasanjaya@gmail.com>
2024-02-01 17:49:51 +0530

    convert in to spring-boot priject

---
+++ eduPanel-back-end-using-Spring-framework-to-simplify-project/src/test/java/lk/ijse/dep11/edupanel/repository/impl/LinkedinRepositoryImplTest.java
@@ -0,0 +1,46 @@
+package lk.ijse.dep11.edupanel.repository.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedinRepositoryImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void existsById() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void count() {
+    }
+}
